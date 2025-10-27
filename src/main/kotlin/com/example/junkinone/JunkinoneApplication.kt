531d7e46560bec24d0a3be69b55eb764b0ad47d4
ekankr2 <ekankr2@naver.com>
2025-10-27 22:28:45 +0900

    init new spring project

---
+++ src/main/kotlin/com/example/junkinone/JunkinoneApplication.kt
@@ -0,0 +1,11 @@
+package com.example.junkinone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JunkinoneApplication
+
+fun main(args: Array<String>) {
+    runApplication<JunkinoneApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/junkinone/JunkinoneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.junkinone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JunkinoneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
